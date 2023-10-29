0b44a48f80d0ff31a11ba530074fce64823f11e7
Ayoub Abedrabbo <Ayoub.Abedrabbo@Gmail.com>
2023-10-28 22:52:12 -0400

    first commit

---
+++ astro.config.mjs
@@ -0,0 +1,7 @@
+import { defineConfig } from 'astro/config';
+
+// https://astro.build/config
+export default defineConfig({
+site: 'https://today20092.github.io',
+base: '/astrowebsitephotography',
+});
