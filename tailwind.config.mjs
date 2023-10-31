1a1a49e664fa81cd42bd4a6f47d8ad77002cdbb8
Ayoub Abedrabbo <Ayoub.Abedrabbo@Gmail.com>
2023-10-31 17:00:13 -0400

    updated comments in tailwind config

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,3 +1,5 @@
+// This is what i had before but it was not building in github or in `npm run build`
+
 // /** @type {import('tailwindcss').Config} */
 // export default {
 // 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
@@ -7,6 +9,7 @@
 // 	plugins: [],
 // }
 
+// This is the section that makes tailwind build with `npm run build` - I am assuming that github will also build because of this section
 
 /** @type {import('tailwindcss').Config} */
 module.exports = {
@@ -16,3 +19,5 @@ module.exports = {
 	},
 	plugins: [],
 };
+
+// -->
