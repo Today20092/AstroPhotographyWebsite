dcb162e5aecc484bfc9a0a43bfe3f0e92906be8c
Ayoub Abedrabbo <Ayoub.Abedrabbo@Gmail.com>
2023-10-31 10:30:27 -0400

    added tailwind

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,10 @@
 import { defineConfig } from 'astro/config';
 
+import tailwind from "@astrojs/tailwind";
+
 // https://astro.build/config
 export default defineConfig({
-site: 'https://today20092.github.io',
-base: '/astrowebsitephotography',
+  site: 'https://today20092.github.io',
+  base: '/astrowebsitephotography',
+  integrations: [tailwind()]
 });
